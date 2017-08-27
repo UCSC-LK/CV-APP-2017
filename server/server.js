e8e898507f1cc13a9f0858d4e67f740245c6146d
Chamath Silva <mbckchamathsilva@gmail.com>
2017-08-27 23:51:25 +0530

    log

--- server/server.js
+++ server/server.js
@@ -70,6 +70,7 @@ app.use(function (err, req, res, next) {
     console.log("Log - UnauthorizedError");
   }else{
     console.log("Log - Unhandlied");
+    console.log("message" + err.name + ": " + err.message);
   }
 });
 
